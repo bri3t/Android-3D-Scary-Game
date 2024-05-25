d224235c45cecbd4e7976254d4119066cd28bc85
Arnau Briet Roura <arnaubriet@gmail.com>
2024-05-25 15:51:31 +0200

    mano pistola y rotacion de la camara smooth

--- 3D Scary Game/Assets/Scripts/PlayerControl.cs
+++ 3D Scary Game/Assets/Scripts/PlayerControl.cs
@@ -3,20 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+// This Will Auto Add Character Controller To Gameobject If It's Not Already Applied:
+[RequireComponent(typeof(CharacterController))]
 public class PlayerControl : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;  // Velocidad de movimiento del personaje
-    public float rotationSpeed = 100.0f;  // Velocidad de rotación de la cámara
+    // Camera:
+    public Camera playerCam;
 
-    private Vector2 moveInput;      // Vector para almacenar la entrada del joystick
-    private Vector2 lookInput;      // Vector para almacenar la entrada de la rotación de la cámara
-    private Camera mainCamera;
+    // Movement Settings:
+    public float walkSpeed = 5.0f;
+    public float runSpeed = 5f;
+    public float jumpPower = 0f;
+    public float gravity = 10f;
+
+    // Camera Settings:
+    public float lookSpeed = 2f;
+    public float lookXLimit = 75f;
+    public float cameraRotationSmooth = 5f;
+
+    // Ground Sounds:
+    public AudioClip[] woodFootstepSounds;
+    public AudioClip[] tileFootstepSounds;
+    public AudioClip[] carpetFootstepSounds;
+    public Transform footstepAudioPosition;
+    public AudioSource audioSource;
+
+    private bool isWalking = false;
+    private bool isFootstepCoroutineRunning = false;
+    private AudioClip[] currentFootstepSounds;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0;
+    private float rotationY = 0;
+
+    private bool canMove = true;
+    private CharacterController characterController;
+
+    private Vector2 moveInput;
+    private Vector2 lookInput;
 
     private PlayerInputActions inputActions;
 
     private void Awake()
     {
-        mainCamera = Camera.main;
+        characterController = GetComponent<CharacterController>();
         inputActions = new PlayerInputActions();
     }
 
@@ -34,9 +64,19 @@ public class PlayerControl : MonoBehaviour
         inputActions.Jugador.Disable();
     }
 
+    private void Start()
+    {
+        currentFootstepSounds = woodFootstepSounds;
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        moveInput = Vector2.zero;
     }
 
     public void OnLook(InputAction.CallbackContext context)
@@ -55,31 +95,91 @@ public class PlayerControl : MonoBehaviour
 
     private void Update()
     {
-        // Movimiento del personaje
+         // Movimiento del personaje
         Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y);
-        movement = mainCamera.transform.TransformDirection(movement);
+        movement = playerCam.transform.TransformDirection(movement);
         movement.y = 0f; // Asegúrate de que el movimiento en el eje Y sea cero
         movement.Normalize();
-        transform.Translate(moveSpeed * movement * Time.deltaTime, Space.World);
+        footstepAudioPosition.Translate(walkSpeed * movement * Time.deltaTime, Space.World);
+        //characterController.Move(walkSpeed * movement * Time.deltaTime);
+
+        // Aplicar gravedad manualmente
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+        //characterController.Move(moveDirection * Time.deltaTime);
 
         // Rotación de la cámara
-        if (lookInput != Vector2.zero)
+        if (canMove)
         {
             RotateCamera(lookInput);
+        }
+
+        // Reproducción de sonidos de pasos
+        if ((movement.x != 0f || movement.z != 0f) && !isWalking && !isFootstepCoroutineRunning)
+        {
+            isWalking = true;
+            StartCoroutine(PlayFootstepSounds(1.3f / walkSpeed));
+        }
+        else if (movement.x == 0f && movement.z == 0f)
+        {
+            isWalking = false;
         }
     }
 
     private void RotateCamera(Vector2 lookInput)
     {
-        float rotationX = -lookInput.y * rotationSpeed * Time.deltaTime;  // Rotación vertical (invertida)
-        float rotationY = lookInput.x * rotationSpeed * Time.deltaTime;   // Rotación horizontal
+        rotationX -= lookInput.y * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
 
-        // Asegura que la cámara no se incline
-        mainCamera.transform.Rotate(new Vector3(rotationX, rotationY, 0), Space.Self);
+        rotationY += lookInput.x * lookSpeed;
 
-        // Clamping the rotation to avoid flipping the camera
-        Vector3 currentEulerAngles = mainCamera.transform.localEulerAngles;
-        currentEulerAngles.z = 0; // No inclinación
-        mainCamera.transform.localEulerAngles = currentEulerAngles;
+        Quaternion targetRotationX = Quaternion.Euler(rotationX, 0, 0);
+        Quaternion targetRotationY = Quaternion.Euler(0, rotationY, 0);
+
+        playerCam.transform.localRotation = Quaternion.Slerp(playerCam.transform.localRotation, targetRotationX, Time.deltaTime * cameraRotationSmooth);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotationY, Time.deltaTime * cameraRotationSmooth);
+    }
+
+    // Reproducción de sonidos de pasos con un retraso basado en la velocidad de movimiento
+    private IEnumerator PlayFootstepSounds(float footstepDelay)
+    {
+        isFootstepCoroutineRunning = true;
+
+        while (isWalking)
+        {
+            if (currentFootstepSounds.Length > 0)
+            {
+                int randomIndex = Random.Range(0, currentFootstepSounds.Length);
+                audioSource.transform.position = footstepAudioPosition.position;
+                audioSource.clip = currentFootstepSounds[randomIndex];
+                audioSource.Play();
+                yield return new WaitForSeconds(footstepDelay);
+            }
+            else
+            {
+                yield break;
+            }
+        }
+
+        isFootstepCoroutineRunning = false;
+    }
+
+    // Detección de la superficie del suelo y configuración de la matriz de sonidos de pasos actual
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wood"))
+        {
+            currentFootstepSounds = woodFootstepSounds;
+        }
+        else if (other.CompareTag("Tile"))
+        {
+            currentFootstepSounds = tileFootstepSounds;
+        }
+        else if (other.CompareTag("Carpet"))
+        {
+            currentFootstepSounds = carpetFootstepSounds;
+        }
     }
 }
